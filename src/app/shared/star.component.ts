90f34635b078783aeed6abfb3be7dd436c129718
IhorMykhaliak <ihor123dn@gmail.com>
2017-10-06 14:46:02 +0300

    module 10

---
+++ src/app/shared/star.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnChanges, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+    selector: 'pm-star',
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css']
+})
+
+export class StarComponent {
+    @Input() rating: number = 4;
+    starWidth: number;
+    @Output() ratingClicked: EventEmitter<string> =
+                new EventEmitter<string>();
+
+    onClick(): void {
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
+    }
+}
